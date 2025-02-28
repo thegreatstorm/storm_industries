74c57b239727b5483e60f4f03d7277018423a43c
TheGreatStorm <thegreatstormx@gmail.com>
2025-02-28 11:35:23 -0600

    Anger its not workign copying from minecraft

--- docker_builds/quake_live/build_image.sh
+++ docker_builds/quake_live/build_image.sh
@@ -1,7 +1,8 @@
 # Get your host's UID and GID
+# Make sure you running on something that isn't uid or GID that is 1000. It will error out.
 export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
 
 # Build the Docker image
-docker build --build-arg UID=$HOST_UID --build-arg GID=$HOST_GID USER=$USER -t quake_live .
+docker build --build-arg UID=$HOST_UID --build-arg GID=$HOST_GID --build-arg USER=$USER -t quake_live .
